f970849a5ba8f17aaa43d0867f6c466a49117da0
JUNO <jrlee0701@kakao.com>
2022-04-20 18:52:44 +0900

    Initial Commit

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by JUNO on 2022/04/11.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
---
+++ Diary/ViewController.swift
@@ -0,0 +1,148 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by JUNO on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    private var diaryList = [Diary]() {
+        didSet{
+            self.saveDiaryList()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureCollectionView()
+        self.loadDiaryList()
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(editDiaryNotification(_:)),
+            name: NSNotification.Name("editDiary"),
+            object: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let writeDiaryViewController = segue.destination as? WriteDiaryViewController {
+            writeDiaryViewController.delegate = self
+        }
+    }
+    
+    private func configureCollectionView(){
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+    
+    private func saveDiaryList(){
+        let data = self.diaryList.map {
+            [
+                "title" : $0.title,
+                "contents" : $0.contents,
+                "date" : $0.date,
+                "isStar" : $0.isStar
+            ]
+        }
+        
+        let userDefault = UserDefaults.standard
+        userDefault.set(data, forKey: "diaryList")
+    }
+    
+    private func loadDiaryList(){
+        let userDefualts = UserDefaults.standard
+        guard let data = userDefualts.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        
+        self.diaryList = data.compactMap{
+            guard let title = $0["title"] as? String else { return nil}
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+        }
+        
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+    }
+    
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+    
+    @objc func editDiaryNotification(_ notification: Notification) {
+        guard let diary = notification.object as? Diary else { return }
+        guard let row = notification.userInfo?["indexPath.row"] as? Int else { return }
+        
+        self.diaryList[row] = diary
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+        self.collectionView.reloadData()
+    }
+}
+
+extension ViewController: WriteDiaryViewDelegate {
+    func didSelectRegister(diary: Diary) {
+        self.diaryList.append(diary)
+        self.diaryList = self.diaryList.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+        self.collectionView.reloadData()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.diaryList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as? DiaryCell else { return UICollectionViewCell() }
+        
+        let diary = self.diaryList[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = dateToString(date: diary.date)
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (UIScreen.main.bounds.width / 2) - 20, height: 200)
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "DiaryDetailViewController") as? DiaryDetailViewController else { return }
+        
+        let diary = self.diaryList[indexPath.row]
+        viewController.diary = diary
+        viewController.indexPath = indexPath
+        viewController.delegate = self
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+extension ViewController: DiaryDetailViewDelegate {
+    func didSelectDelete(indexPath: IndexPath) {
+        self.diaryList.remove(at: indexPath.row)
+        self.collectionView.deleteItems(at: [indexPath])
+    }
+    
+    func didSelectStar(indexPath: IndexPath, isStar: Bool) {
+        self.diaryList[indexPath.row].isStar = isStar
+    }
+}
